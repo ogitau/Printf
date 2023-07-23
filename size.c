a5773ed2af0dad5463ee497404c672518a2f1043
FAITH RONO <132823200+FaithRono@users.noreply.github.com>
2023-07-23 22:57:06 +0300

    Update size.c

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
+ * @format: string in which to print the arguments
  * @i: List of arguments to be printed.
  *
  * Return: Precision.
