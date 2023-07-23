2af26abb0b3739b90c20b1869fa82898ea959d7f
FAITH RONO <132823200+FaithRono@users.noreply.github.com>
2023-07-23 22:44:00 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
+ * @format: string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
