2174e3583a2559a4c02e7793ec61d4f71fe6b35c
FAITH RONO <132823200+FaithRono@users.noreply.github.com>
2023-07-23 22:42:09 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * @format: string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
