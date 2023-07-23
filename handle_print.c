fdcdf200925f98d9c265a3a9df0d2075b985fb72
FAITH RONO <132823200+FaithRono@users.noreply.github.com>
2023-07-23 22:45:10 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
+ * @fmt: string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
